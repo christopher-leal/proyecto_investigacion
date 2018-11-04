ffda5fdd87026a3b44155f06ad7a028e2a225f64
Alan_Cruz <roblesvazquez@tecmor.mx>
2018-11-04 00:25:21 -0600

    conexion con base de datos

---
+++ php/conexion.php
@@ -0,0 +1,29 @@
+<?php
+$sqlUser="root";
+$sqlPass="";
+
+function iniciaConexion(){
+  GLOBAL $sqlUser, $sqlPass;
+  $conexionMySQL = @mysqli_connect("localhost", $sqlUser, $sqlPass, "pgpi");
+  if (!$conexionMySQL) {
+    return null;
+    die();
+  }
+  return $conexionMySQL;
+}
+
+function consultaSQL($consunta){
+  $datos=array();
+  $conexionMySQL = iniciaConexion();
+  if(!$resultado = @mysqli_query($conexionMySQL, $consunta)){
+    return null;
+    die();
+  }
+  $i=0;
+  while($row = mysqli_fetch_assoc($resultado))
+  {
+    $datos[$i] = $row;
+    $i++;
+  }
+  return $datos;
+}
