538e0964f3db2734aa1743f17337f02b774c9078
Christopher Leal <chris_t_o@hotmail.com>
2018-11-13 17:55:24 -0600

    Se agrego el cambio de la base de datos

--- includes/funciones/proyectos_bd.php
+++ includes/funciones/proyectos_bd.php
@@ -1,7 +1,8 @@
 <?php 
     include_once 'bdconexion.php';
     try {
-        $sql = "select p.id_proyecto, p.titulo_proyecto,p.linea_investigacion,p.fecha_inicio, p.fecha_fin,  p.link_imagen, p.resumen, i.nombre, i.apellido_paterno, i.apellido_materno from proyectos p inner join investigadores i on p.id_investigador=i.id_investigador;";
+        // $sql = "select p.id_proyecto, p.titulo_proyecto,p.linea_investigacion,p.fecha_inicio, p.fecha_fin,  p.link_imagen, p.resumen, i.nombre, i.apellido_paterno, i.apellido_materno from proyectos p inner join investigadores i on p.id_investigador=i.id_investigador;";
+        $sql = "select p.id_proyecto, p.titulo_proyecto, p.linea_investigacion, p.fecha_inicio,p.fecha_fin, p.link_imagen, p.resumen, i.nombre, i.apellido_paterno, i.apellido_materno from proyectos p inner join investigadores i on p.lider_proyecto=i.id_investigador;";
         $datos = $conn->query($sql);
         while($dato=$datos->fetch_assoc()){
             $informacion=array(
@@ -14,7 +15,7 @@
                 'resumen'=>$dato['resumen'],
                 'nombre'=>$dato['nombre'],
                 'apellido_paterno'=>$dato['apellido_paterno'],
-                'apellido_materno'=>$dato['apellido_materno']            );
+                'apellido_materno'=>$dato['apellido_materno']           );
            $info[]=$informacion;
         }
         echo json_encode($info);
