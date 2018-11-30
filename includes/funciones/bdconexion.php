b409911d9a0a24b78a0e077e25d1342815ae1d8b
Christopher Leal <chris_t_o@hotmail.com>
2018-11-30 13:15:42 -0600

    Se agregaron las imagenes con menor tamaño

--- includes/funciones/bdconexion.php
+++ includes/funciones/bdconexion.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli('localhost','root','','pgpi');
+    $conn = new mysqli('us-cdbr-iron-east-01.cleardb.net','b885830d782d42','22f7f91f','heroku_8f86bb4b3e24a4c');
 
     if($conn->connect_error) {
         echo $error->$conn->connect_error;
