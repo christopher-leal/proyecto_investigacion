cf40700b2d0b55c24da5641bd636b6807f2f9152
Christopher Leal <chris_t_o@hotmail.com>
2018-12-06 17:42:14 -0600

    comentado para poder subir al servidor

--- includes/funciones/bdconexion.php
+++ includes/funciones/bdconexion.php
@@ -1,6 +1,6 @@
 <?php
-    // $conn = new mysqli('us-cdbr-iron-east-01.cleardb.net','b885830d782d42','22f7f91f','heroku_8f86bb4b3e24a4c');
-    $conn = new mysqli('localhost','root','','pgpi');
+    $conn = new mysqli('us-cdbr-iron-east-01.cleardb.net','b885830d782d42','22f7f91f','heroku_8f86bb4b3e24a4c');
+    // $conn = new mysqli('localhost','root','','pgpi');
     if($conn->connect_error) {
         echo $error->$conn->connect_error;
     }
